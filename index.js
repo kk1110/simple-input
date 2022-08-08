32cd9d5dcebc12c1ba4ce844608115f0a92e6881
Siddeeq Kamal <kayalkamal1110@gmail.com>
2022-08-08 11:58:07 +0530

    small changes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const validateInput= function(){
     var input = document.getElementById("inputField").value;
+    var message = document.getElementById("message")
      
     if(input<0){
-        document.getElementById("message").innerHTML="Enter Positive value";
+        message.innerHTML="Enter Positive value";
     }
         
    else if((input>0) && ((input%2===0)||(input%2!==0))){
         let value = parseInt(input);
         let output=[value+2,value+4,value+6];
-        document.getElementById("message").innerHTML=output;
+        message.innerHTML=output;
     }
 
     else{
-        document.getElementById("message").innerHTML="";
+        message.innerHTML="";
     }
 }
